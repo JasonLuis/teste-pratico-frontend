080942b186cf96fa279a7f9209a3eeecda2d7190
JasonLuis <jason20luis@gmail.com>
2025-02-28 18:02:17 -0300

    fix: ajusta dados de comparação no teste

--- frontend/src/app/modules/employees/use-cases/GetAll/GetAllEmployees.spec.ts
+++ frontend/src/app/modules/employees/use-cases/GetAll/GetAllEmployees.spec.ts
@@ -1,3 +1,4 @@
+import { Helper } from "@/app/shared/helper/Helper";
 import { employeeUseCase } from ".";
 
 describe('Use Case: Employees/GetAllEmployees', () => {
@@ -10,8 +11,8 @@ describe('Use Case: Employees/GetAllEmployees', () => {
             id: 1,
             name: "João",
             job: "Back-end",
-            admission_date: "2019-12-02T00:00:00.000Z",
-            phone: "5551234567890",
+            admission_date: Helper.DateMask("2019-12-02T00:00:00.000Z"),
+            phone: Helper.PhoneMask("5551234567890"),
             image: "https://img.favpng.com/25/7/23/computer-icons-user-profile-avatar-image-png-favpng-LFqDyLRhe3PBXM0sx2LufsGFU.jpg"
         };
 
